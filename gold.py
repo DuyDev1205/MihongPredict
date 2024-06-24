996d9cf30a52b44e1cc9f0f41e784e6317c9a68a
Trần Thanh Duy <DUY.TT22714@sinhvien.hsu.edu.vn>
2024-06-24 13:46:36 +0700

    add function print data from the lastest value in API

--- gold.py
+++ gold.py
@@ -19,3 +19,19 @@ def get_latest_data(data):
         date_str = entry['date'].split()[0]  # Lấy phần ngày từ chuỗi 'date'
         latest_data[date_str] = entry  # Ghi đè giá trị, chỉ giữ lại giá trị cuối cùng
     return list(latest_data.values())
+def print_data():
+    start_date = datetime(2024, 4, 2, 0, 0, 0).strftime("%d/%m/%Y %H:%M:%S")
+    end_date = datetime(2024, 4, 10, 23, 59, 59).strftime("%d/%m/%Y %H:%M:%S")
+    response = get_gold_API(start_date, end_date)
+    if response and response['success']:
+        data = response['data']
+        latest_data = get_latest_data(data)
+        for val in latest_data:
+            json_data = {
+                'sell': val['sell'],
+                'date': datetime.strptime(val['date'], "%Y/%m/%d %H:%M:%S").strftime("%d/%m/%Y %H:%M:%S")
+            }
+            formatted_json = json.dumps(json_data, indent=4, ensure_ascii=False)
+            print(formatted_json)
+    else:
+        print("Không có dữ liệu hoặc yêu cầu không thành công.")
